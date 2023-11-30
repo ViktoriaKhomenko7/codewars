5ebcf169f91e9321fc80c48c4955b9f985b40be0
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-11-30 20:10:54 +0300

    Keep Hydrated

--- main.js
+++ main.js
@@ -27,11 +27,20 @@
 // 3. Your task is to create a function that does four basic mathematical operations.
 // The function should take three arguments - operation(string/char), value1(number), value2(number).
 // The function should return result of numbers after applying the chosen operation.
-function basicOp(operation, value1, value2){
-    switch(operation) {
-        case "+" : return value1 + value2;
-        case "-" : return value1 - value2;
-        case "*" : return value1 * value2;
-        case "/" : return value1 / value2;
-    } 
-  }
+// 
+//function basicOp(operation, value1, value2){
+//     switch(operation) {
+//         case "+" : return value1 + value2;
+//         case "-" : return value1 - value2;
+//         case "*" : return value1 * value2;
+//         case "/" : return value1 / value2;
+//     } 
+//   }
+
+
+//4.Keep Hydrated
+//Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+//You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+// function litres(time) {
+//     return Math.floor(time * 0.5);
+// }
