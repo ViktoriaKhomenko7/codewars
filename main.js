e0ea3a6deb35d8f76d1834d822aef97f5d902163
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-11-30 19:17:29 +0300

    even or odd

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //Consider an array/list of sheep where some sheep may be missing from their place. 
 //We need a function that counts the number of sheep present in the array (true means present).
 //solution
-function countSheeps(sheep) {
-    let result = 0;
-    for (let i = 0; i < sheep.length; i++) {
-        if(sheep[i]) {
-            result++
-        }
-    }
-    return result
-}
+// function countSheeps(sheep) {
+//     let result = 0;
+//     for (let i = 0; i < sheep.length; i++) {
+//         if(sheep[i]) {
+//             result++
+//         }
+//     }
+//     return result
+// }
