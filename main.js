b677f743dd22fa254def9c88b999865727b05263
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-11-30 19:38:12 +0300

    basic mathematical operations

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Consider an array/list of sheep where some sheep may be missing from their place. 
+//1. Consider an array/list of sheep where some sheep may be missing from their place. 
 //We need a function that counts the number of sheep present in the array (true means present).
 //solution
 // function countSheeps(sheep) {
@@ -13,12 +13,25 @@
 
 
 
-// Even or Odd
+// 2. Even or Odd
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-function evenOrOdd(number) {
-    if(number%2===0) {
-      return "Even"
-    } else {
-      return "Odd"
-    }
+// function evenOrOdd(number) {
+//     if(number%2===0) {
+//       return "Even"
+//     } else {
+//       return "Odd"
+//     }
+//   }
+
+
+// 3. Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+function basicOp(operation, value1, value2){
+    switch(operation) {
+        case "+" : return value1 + value2;
+        case "-" : return value1 - value2;
+        case "*" : return value1 * value2;
+        case "/" : return value1 / value2;
+    } 
   }
