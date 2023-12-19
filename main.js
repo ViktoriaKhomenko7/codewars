cfd0be73aaa8a281ebe12490643ec07a680e8e1c
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-12-19 16:47:20 +0300

    training

--- main.js
+++ main.js
@@ -41,6 +41,41 @@
 //4.Keep Hydrated
 //Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
 //You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
-// function litres(time) {
-//     return Math.floor(time * 0.5);
+
+function litres(time) {
+    return Math.floor(time * 0.5);
+}
+
+
+//5. Sum of positive
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+// function positiveSum(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         if (arr[i] > 0) {
+//             sum += arr[i]
+//         }
+//     }
+//     return sum
 // }
+
+function positiveSum(arr) {
+    return arr.reduce((sum, num) => sum + (num > 0 ? num : 0), 0)
+}
+
+
+
+//6. Return negative
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+// function makeNegative(num) {
+//     return -Math.abs(num)
+// }
+
+function makeNegative(num) {
+    return num > 0 ? -num : num
+}
+
+
+//7.
