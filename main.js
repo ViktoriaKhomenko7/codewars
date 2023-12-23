acabbd392a4e370840b40322412edb4ee8d0a7d1
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-12-23 20:23:48 +0300

    a Boolean to a String

--- main.js
+++ main.js
@@ -291,8 +291,19 @@
 //28.Beginner Series #1 School Paperwork
 //Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 //Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
-function paperwork(n, m) {
-    if(n>=0 && m>=0) return n*m
-    if(n<0 || m<0) return 0
-  }
-  
+// function paperwork(n, m) {
+//     if(n>=0 && m>=0) return n*m
+//     if(n<0 || m<0) return 0
+//   }
+
+// function paperwork(n, m) {
+//     return n > 0 && m > 0 ? n * m : 0
+//   }
+
+
+
+//29. Convert a Boolean to a String
+function booleanToString(b){
+    return b.toString()
+}
+
