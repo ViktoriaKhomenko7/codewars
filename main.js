68e985d634129467beee5c242941a341ecc255f8
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-12-23 20:00:40 +0300

    Opposites Attract

--- main.js
+++ main.js
@@ -264,6 +264,19 @@
 //     }
 
 //25. Function 1 - hello world
-const greet = () => {
-    return "hello world"
-}
+// const greet = () => {
+//     return "hello world"
+// }
+
+
+//.26 Opposites Attract
+//Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+function lovefunc(flower1, flower2){
+    return flower1 % 2 !== flower2 % 2 ? true : false
+  }
+
+  function lovefunc(flower1, flower2){
+    if(flower1 % 2 === 0 && flower2 %2 === 1) return true
+    if (flower2 % 2 === 0 && flower1 % 2 === 1) return true
+    return false
+  }
