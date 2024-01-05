c68c25faa7e192731bc543e2fd72bcbcc432ac67
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2024-01-05 18:29:17 +0300

    Count the divisors of a number2

--- main.js
+++ main.js
@@ -414,11 +414,25 @@
 //7 kyu
 //40. Count the divisors of a number
 //Count the number of divisors of a positive integer n. Random tests go up to n = 500000.
+// function getDivisorsCnt(n){
+//   let result = 0;
+//   for(let i = 1; i <= n; i++){
+//     if(n % i===0){
+//       result++
+//     }
+//   }
+//   return result
+// }
+// ЭТОТ СЛИШКОМ ДОЛГИЙ
+
 function getDivisorsCnt(n){
   let result = 0;
-  for(let i = 1; i <= n; i++){
-    if(n % i===0){
+  if(n % Math.sqrt(n) == 0){
       result++
+    }
+  for(let i = 0; i < Math.sqrt(n); i++){
+    if(n%i === 0){
+      result += 2
     }
   }
   return result
