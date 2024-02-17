a1d6384ab574d9bff6a69622917e49894f76187e
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2024-02-17 12:43:29 +0300

    Multiples of 3 or 5

--- main.js
+++ main.js
@@ -637,17 +637,57 @@
 //s="aaaxbbbbyyhwawiwjjjwwm"
 //printer_error(s) => "8/22"
 
-function printerError(s) {
-  let count = 0;
-for(let i = 0; i < s.length; i++){
-  if(s[i] > 'm'){
-    count++
-  }
-}
-return count + "/" + s.length
-}
+// function printerError(s) {
+//   let count = 0;
+// for(let i = 0; i < s.length; i++){
+//   if(s[i] > 'm'){
+//     count++
+//   }
+// }
+// return count + "/" + s.length
+// }
 
-function printerError(s) {
-  let b = Array.from(s).filter(letter => letter > "m");
-  return b.length+"/"+s.length;
+// function printerError(s) {
+//   let b = Array.from(s).filter(letter => letter > "m");
+//   return b.length+"/"+s.length;
+// }
+
+
+// const foo = () => {
+//   const randomNum = Math.random()*11
+//   const result = Math.round(randomNum)
+//   return result
+// }
+
+// const a = foo()
+// console.log(a);
+
+// // function getRandomNumber() {
+// //   return Math.floor(Math.random() * 11);
+// // }
+
+// // const randomNumber = getRandomNumber();
+// // console.log(randomNumber);
+
+
+//61. 6kyu
+//Multiples of 3 or 5
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+//Additionally, if the number is negative, return 0.
+//Note: If the number is a multiple of both 3 and 5, only count it once.
+function solution(number){
+  let sum = 0;
+  for(let i = 0; i < number; i++){
+    if(i % 3 == 0){
+      sum += i
+      continue
+    }
+    if(i % 5 == 0){
+      sum += i
+      continue
+    }
+    else { continue}
+  }
+  return sum
 }
