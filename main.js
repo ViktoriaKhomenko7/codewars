5328a3cc478206c2c633a73e721bc86ecacaf8e5
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2024-02-29 20:40:16 +0300

    createPhoneNumber

--- main.js
+++ main.js
@@ -760,16 +760,28 @@
 //We want to create the text that should be displayed next to such an item.
 //Implement the function which takes an array containing the names of people 
 //that like an item. It must return the display text as shown in the examples:
-function likes(names) {
-  if(names.length == 0){
-    return "no one likes this"
-  } else if(names.length == 1){
-    return names[0] + " likes this"
-  } else if(names.length == 2){
-    return names[0] + " and " +  names[1] + " like this"
-  } else if(names.length == 3){
-    return names[0] + ", " +  names[1] + " and " + names[2] + " like this"
-  } else {
-    return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this"
-  }
+// function likes(names) {
+//   if(names.length == 0){
+//     return "no one likes this"
+//   } else if(names.length == 1){
+//     return names[0] + " likes this"
+//   } else if(names.length == 2){
+//     return names[0] + " and " +  names[1] + " like this"
+//   } else if(names.length == 3){
+//     return names[0] + ", " +  names[1] + " and " + names[2] + " like this"
+//   } else {
+//     return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this"
+//   }
+// }
+
+
+//65.createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// function createPhoneNumber(numbers){
+//   return "(" + `${numbers[0]}${numbers[1]}${numbers[2]}` + ")" + " " + `${numbers[3]}${numbers[4]}${numbers[5]}` + "-" + `${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+// }
+function createPhoneNumber(numbers){
+  numbers.unshift("(");
+  numbers.splice(4, 0, ")", " ");
+  numbers.splice(9, 0, "-");
+  return numbers.join("");
 }
